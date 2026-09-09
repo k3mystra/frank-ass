3453a87d600241160d3da846532efda1c0582328
skyking336 <3336jason@gmail.com>
2026-09-09 13:40:22 +0800

    feat(player): implement first-person controller, basic test room, and configure Git LFS

---
+++ scripts/player/player_controller.gd
@@ -0,0 +1,52 @@
+class_name PlayerController
+extends CharacterBody3D
+
+
+@export var speed: float = 4.5
+@export var sprint_speed: float = 6.5
+@export var jump_velocity: float = 4.0
+@export var mouse_sensitivity: float = 0.0025
+
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var head: Node3D = $Head
+@onready var camera: Camera3D = $Head/Camera3D
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void:
+
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		head.rotate_x(-event.relative.y * mouse_sensitivity)
+		head.rotation.x = clampf(head.rotation.x, deg_to_rad(-85.0), deg_to_rad(85.0))
+
+	if event.is_action_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0.0, input_dir.y)).normalized()
+
+	var is_sprinting: bool = Input.is_action_pressed("sprint") if InputMap.has_action("sprint") else Input.is_key_pressed(KEY_SHIFT)
+	var current_speed: float = sprint_speed if is_sprinting else speed
+
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0.0, current_speed)
+		velocity.z = move_toward(velocity.z, 0.0, current_speed)
+
+	move_and_slide()
