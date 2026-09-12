63ea5db10d9623c14e8824ed020a24d8b011b3b2
k3mystra <programmer.coleslaw981@slmail.me>
2026-09-12 10:04:37 +0800

    feat: Connect socket signals to device

--- scripts/devices/device.gd
+++ scripts/devices/device.gd
@@ -1,6 +1,13 @@
 extends Node
 
 @export var associated_metric: Metric
+@export var associated_socket: BatterySocket
+
+
+func _ready() -> void:
+    associated_socket.battery_attached.connect(_on_battery_attached)
+    associated_socket.battery_detached.connect(_on_battery_detached)
+    associated_socket.battery_depleted.connect(_on_battery_depleted)
 
 
 func _on_battery_attached():
