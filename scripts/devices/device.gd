11e28819bfc8c7e1fca34d4f57e7399963f079fa
k3mystra <programmer.coleslaw981@slmail.me>
2026-09-13 12:02:34 +0800

    formatting

--- scripts/devices/device.gd
+++ scripts/devices/device.gd
@@ -5,18 +5,19 @@ extends Node
 
 
 func _ready() -> void:
-    associated_socket.battery_attached.connect(_on_battery_attached)
-    associated_socket.battery_detached.connect(_on_battery_detached)
-    associated_socket.battery_depleted.connect(_on_battery_depleted)
+	associated_socket.battery_attached.connect(_on_battery_attached)
+	associated_socket.battery_detached.connect(_on_battery_detached)
+	associated_socket.battery_depleted.connect(_on_battery_depleted)
 
 
-func _on_battery_attached(_battery_data: BatteryData):
-    EventBus.power_up.emit(associated_metric.name)
+func _on_battery_attached(battery_data: BatteryData):
+	if battery_data.charge > 0:
+		EventBus.power_up.emit(associated_metric.name)
 
 
 func _on_battery_detached(_battery_data: BatteryData):
-    EventBus.power_down.emit(associated_metric.name)
+	EventBus.power_down.emit(associated_metric.name)
 
 
 func _on_battery_depleted(_battery_data: BatteryData):
-    EventBus.power_down.emit(associated_metric.name)
+	EventBus.power_down.emit(associated_metric.name)
