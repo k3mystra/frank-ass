401e119c308ab7930e4de392063ec063599181d6
k3mystra <programmer.coleslaw981@slmail.me>
2026-09-13 23:06:25 +0800

    feat: menu rotating The Game

--- scripts/core/menu.gd
+++ scripts/core/menu.gd
@@ -1,7 +1,11 @@
 extends Node
 
 @export var game_scene: PackedScene
+@export_range(0, 1) var stop_chance: float
 
+@onready var rng = RandomNumberGenerator.new()
+@onready var anim_player = $AnimationPlayer
+@onready var stop_timer = $StopTimer
 
 func _on_exit_btn_pressed() -> void:
 	get_tree().quit()
@@ -9,3 +13,14 @@ func _on_exit_btn_pressed() -> void:
 
 func _on_play_btn_pressed() -> void:
 	SceneManager.goto_scene(game_scene.resource_path)
+
+
+func _on_stop_timer_timeout() -> void:
+	anim_player.play()
+
+
+func _on_cycle_timer_timeout() -> void:
+	var value = rng.randf()
+	if value < stop_chance and stop_timer.is_stopped():
+		anim_player.pause()
+		stop_timer.start()
